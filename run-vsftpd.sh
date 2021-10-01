76c2b6d729b2dbe52bc5d98234397d033ea93c57
mrh929 <809999463@qq.com>
2021-10-01 20:30:11 +0800

    provide options for skipping chmod

--- run-vsftpd.sh
+++ run-vsftpd.sh
@@ -19,7 +19,11 @@ fi
 
 # Create home dir and update vsftpd user db:
 mkdir -p "/home/vsftpd/${FTP_USER}"
-chown -R ftp:ftp /home/vsftpd/
+
+if [ "$CHMOD_DIRECTORY" = "YES" ]; then
+	# If you are using rclone, you can skip this command:
+	chown -R ftp:ftp /home/vsftpd/
+fi
 
 echo -e "${FTP_USER}\n${FTP_PASS}" > /etc/vsftpd/virtual_users.txt
 /usr/bin/db_load -T -t hash -f /etc/vsftpd/virtual_users.txt /etc/vsftpd/virtual_users.db
